467dfa51cd1e707bd3e54f492b0d82c06258cf62
codingvaleria <codingvaleria@gmail.com>
2023-02-03 14:50:36 +0300

    Manually set timer

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,6 @@
 //See the timer increment every second once the page has loaded.
 let counter = document.getElementById("counter");
-let counternumber = counter.textContent
+let counternumber = counter.textContent;
 console.log(counternumber);
 
 let seconds = 0;
@@ -12,6 +12,32 @@ function count() {
   }, 1000);
 }
 
-document.addEventListener("DOMContentLoaded", count);
+// document.addEventListener("DOMContentLoaded", count);
 
+// Manually increment and decrement the counter using the plus and minus buttons.
+let addBtn = document.querySelector("#plus");
+addBtn.addEventListener("click", increment);
 
+let btnClick = 0;
+
+function increment() {
+  btnClick += 1;
+  counter.textContent = btnClick;
+}
+
+let subtractBtn = document.querySelector("#minus");
+
+subtractBtn.addEventListener("click", decrement);
+
+function decrement() {
+  btnClick -= 1;
+  counter.textContent = btnClick;
+}
+
+// "Like" an individual number of the counter. I should see the count of the number of "likes" associated with that number displayed.
+// let likeBtn = document.querySelector("#heart");
+// likeBtn.addEventListener("click", likesCount);
+
+// function likesCount() {
+//   console.log ("likes")
+// }
